01c44e392dcabe9a23f2eaf5f755d562679c5a84
Shuo Han <1491566796@qq.com>
2023-08-13 12:46:27 -0500

    Update gym_env.py

--- gym_env.py
+++ gym_env.py
@@ -9,47 +9,45 @@ from gymnasium import spaces
 import numpy as np
 
 
-
 class Environment(Env):
-    def __init__(self, world_name: str, freq: int = 100, has_predator: bool = False, real_time: bool = False, prey_agent:Agent=None, max_step = 200):
+    def __init__(self, e=5,
+                 freq: int = 100,
+                 has_predator: bool = False,
+                 real_time: bool = False,
+                 prey_agent: Agent = None,
+                 max_step: int = 200,
+                 predator_speed: float = 1.0,
+                 env_type: str = "train"):
+        if env_type == "train":
+            world_name = "%02i_%02i" % (random.randint(0, 19), e)
+        else:
+            world_name = "%02i_%02i" % (random.randint(20, 21), e)
+        self.freq = freq
+        self.real_time = real_time
+        self.prey_agent = prey_agent
+        self.env_type = env_type
+        self.e = e
         self.world = World.get_from_parameters_names("hexagonal", "canonical", world_name)
-        self.model = Model(pworld=self.world, freq=freq, real_time=real_time)
+        self.model = Model(pworld=self.world, freq=self.freq, real_time=self.real_time)
         self.goal_location = Location(1, .5)
         self.start_location = Location(0, .5)
-        self.observation_space = spaces.Box(-np.inf, np.inf, (4,), dtype=np.float32)
+        self.observation_space = spaces.Box(-np.inf, np.inf, (11,), dtype=np.float32)
+        if has_predator:
+            self.observation_space = spaces.Box(-np.inf, np.inf, (14,), dtype=np.float32)
         self.action_space = spaces.Box(-1, 1, (2,), np.float32)
         self.has_predator = has_predator
         self.max_step = max_step
         self.current_step = 0
         self.episode_reward_history = []
         self.current_episode_reward = 0
+        self.predator = None
+        self.predator_speed = predator_speed
         self.goal_threshold = self.world.implementation.cell_transformation.size / 2
         self.capture_threshold = self.world.implementation.cell_transformation.size
-        self.model.add_agent("prey", prey_agent, Location(0,0), 0, "b", pauto_update=prey_agent is not None)
         self.goal_area = self.model.display.circle(location=self.goal_location,
                                                    color="g",
                                                    alpha=.5,
                                                    radius=self.goal_threshold)
-        self.predator = None
-        if has_predator:
-            paths_builder = Paths_builder.get_from_name("hexagonal", world_name)
-            self.predator = Predator(self.world,
-                                     ppath_builder=paths_builder,
-                                     pvisibility=self.model.visibility,
-                                     pP_value=2,
-                                     pI_value=0,
-                                     pD_value=0,
-                                     pmax_speed=.15,
-                                     pmax_turning_speed=math.pi)
-
-            self.spawn_locations = Location_list()
-            for c in self.world.cells.free_cells():
-                if not self.model.visibility.is_visible(self.start_location, c.location):
-                    self.spawn_locations.append(c.location)
-            self.model.add_agent("predator", self.predator, Location(0, 0), 0, "r")
-            self.predator_destination = self.model.display.circle(location=Location(0,0), color="b", alpha=.5, radius=0.01)
-            self.predator_destination_cell = self.model.display.circle(location=Location(0,0), color="g", alpha=.5, radius=0.02)
-            self.predator_capture_area = self.model.display.circle(location=Location(0,0), color="r", alpha=.5, radius=self.capture_threshold)
 
     def is_goal_reached(self, prey_location: Location):
         return prey_location.dist(self.goal_location) <= self.goal_threshold
@@ -57,12 +55,25 @@ class Environment(Env):
     def is_prey_captured(self, prey_location: Location, predator_location: Location):
         return prey_location.dist(predator_location) <= self.capture_threshold
 
+
     def get_observation(self) -> tuple:
         o = self.model.get_observation("prey")
         prey = o["prey"]
         goal_reached = self.is_goal_reached(prey.location)
         if goal_reached:
             self.complete = True
+
+        # occlusions = Location_list([c.location for c in self.world.cells if c.occluded])
+        occlusions = Location_list([(c.location.dist(prey.location), c.location) for c in self.world.cells if c.occluded])
+        occlusions.sort(key=lambda a: a[0])
+
+        def dif_dir(a,b):
+            diff, direc = angle_difference(a, b)
+            return diff * direc
+
+        max_number_of_occlusions_in_observation = 3
+        closest_occlusions = [(c[0], dif_dir(prey.location.atan(c[1]), prey.theta)) for c in occlusions[:min(len(occlusions), max_number_of_occlusions_in_observation)]]
+
         if self.has_predator:
             if o["predator"]:
                 predator = o["predator"]
@@ -75,7 +86,8 @@ class Environment(Env):
                        predator.location, \
                        predator.theta, \
                        captured, \
-                       goal_reached
+                       goal_reached, \
+                       closest_occlusions
             else:
                 return prey.location, \
                        prey.theta, \
@@ -83,9 +95,11 @@ class Environment(Env):
                        None, \
                        None, \
                        False, \
-                       goal_reached
+                       goal_reached, \
+                       closest_occlusions
         else:
-            return prey.location, prey.theta, self.goal_location, goal_reached
+            return prey.location, prey.theta, self.goal_location, goal_reached, closest_occlusions
+
 
     def is_complete(self):
         return self.complete
@@ -111,12 +125,17 @@ class Environment(Env):
         speed, turning = action[0], action[1]
         self.set_action(speed, turning)
         self.model.step()
-        location, theta, goal_location, goal_reached = self.get_observation()
-        obs = np.array([location.x, location.y, goal_location.x, goal_location.y], dtype=np.float32)
+        location, theta, goal_location, goal_reached, closest_occlusions = self.get_observation()
+        closest_distance,  closest_angle = closest_occlusions[0]
+        se_closest_distance, se_closest_angle = closest_occlusions[1]
+        th_closest_distance, th_closest_angle = closest_occlusions[2]
+        obs = np.array(
+            [location.x, location.y, theta, 0.0, 0.0, closest_distance, closest_angle, se_closest_distance,
+             se_closest_angle, th_closest_distance, th_closest_angle],
+            dtype=np.float32)
         dx = abs(obs[0] - 1)
         dy = abs(obs[1] - 0.5)
         d = math.sqrt(dx ** 2 + dy ** 2)
-        # d_reward = -d
         if self.is_goal_reached(location):
             reward = 100
             done = True
@@ -132,10 +151,60 @@ class Environment(Env):
             self.current_episode_reward = 0
         return obs, reward, done, truncated, info
 
-    def reset(self, seed = None):
+    def reset(self, seed=None):
+        import matplotlib.pyplot as plt
+        plt.close(fig=None)
+        e = self.e  # Assuming 'e' is an instance variable
+        if self.env_type == "train":
+            world_name = "%02i_%02i" % (random.randint(0, 19), e)
+        else:
+            world_name = "%02i_%02i" % (random.randint(20, 21), e)
+        self.world = World.get_from_parameters_names("hexagonal", "canonical", world_name)
+        self.model = Model(pworld=self.world, freq=self.freq, real_time=self.real_time)
+        self.model.add_agent("prey",
+                             self.prey_agent,
+                             Location(0, 0), 0, "b",
+                             pauto_update=self.prey_agent is not None)
+        # if self.has_predator:
+        #     paths_builder = Paths_builder.get_from_name("hexagonal", world_name)
+        #     self.predator = Predator(self.world,
+        #                              ppath_builder=paths_builder,
+        #                              pvisibility=self.model.visibility,
+        #                              pP_value=2,
+        #                              pI_value=0,
+        #                              pD_value=0,
+        #                              pmax_speed=self.predator_speed,
+        #                              pmax_turning_speed=math.pi)
+        #
+        #     self.spawn_locations = Location_list()
+        #     for c in self.world.cells.free_cells():
+        #         if not self.model.visibility.is_visible(self.start_location, c.location):
+        #             self.spawn_locations.append(c.location)
+        #     self.model.add_agent("predator", self.predator, Location(0, 0), 0, "r")
+        #     self.predator_destination = self.model.display.circle(location=Location(0,0),
+        #                                                           color="b",
+        #                                                           alpha=.5,
+        #                                                           radius=0.01)
+        #     self.predator_destination_cell = self.model.display.circle(location=Location(0,0),
+        #                                                                color="g",
+        #                                                                alpha=.5,
+        #                                                                radius=0.02)
+        #     self.predator_capture_area = self.model.display.circle(location=Location(0,0),
+        #                                                            color="r",
+        #                                                            alpha=.5,
+        #                                                            radius=self.capture_threshold)
         self.start()
-        location, theta, goal_location, goal_reached = self.get_observation()
-        obs = np.array([location.x, location.y, goal_location.x, goal_location.y], dtype=np.float32)
+        location, theta, goal_location, goal_reached, closest_occlusions = self.get_observation()
+        closest_distance,  closest_angle = closest_occlusions[0]
+        se_closest_distance, se_closest_angle = closest_occlusions[1]
+        th_closest_distance, th_closest_angle = closest_occlusions[2]
+        obs = np.array(
+            [location.x, location.y, theta,
+             0.0, 0.0,
+             closest_distance, closest_angle,
+             se_closest_distance, se_closest_angle,
+             th_closest_distance, th_closest_angle],
+            dtype=np.float32)
         self.current_step = 1
         self.stop()
         return obs, {}
