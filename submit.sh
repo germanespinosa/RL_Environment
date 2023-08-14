cff18fe14af27066f609ac5ef69927aa35350626
Shuo Han <1491566796@qq.com>
2023-08-13 21:41:08 -0500

    Create submit.sh

---
+++ submit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -A p31777
+#SBATCH -p gengpu
+#SBATCH --gres=gpu:a100:1
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -t 12:00:00
+#SBATCH --mem=40G
+#SBATCH --job-name="Shuo_han_diffusion_video\${SLURM_ARRAY_TASK_ID}" ## use the task id in the name of the job
+#SBATCH --mail-type=ALL ## you can receive e-mail alerts from SLURM when your job begins and when your job finishes (completed, failed, etc)
+#SBATCH --mail-user=shuohan2024@u.northwestern.edu  ## your email
+
+
+
+echo "Running on a single CPU core"
+
+source ~/.bashrc
+conda activate torchenv
+python sac_dense.py
+
+date
