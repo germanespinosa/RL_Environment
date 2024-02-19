40139bddd0c14a96bf2e450185c95606b9185668
Shuo Han <1491566796@qq.com>
2024-02-19 15:38:47 -0600

    Update __init__.py

--- prey_env/__init__.py
+++ prey_env/__init__.py
@@ -4,5 +4,5 @@ sys.path.append('./prey_env')
 from .gym_env import Environment as gymnasium_Environment_C
 from .gym_bins_env import Environment as gym_Environment_D
 from .gymnasium_env_bins import Environment as gymnasium_Environment_D
-
-
+from .MultiAgentEnv import MultiAgentEnvironment
+from .lppo_env import LPPOEnvironment
